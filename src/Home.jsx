3c0585b78d387ec89a1fa716df14790393984a35
Zoukicode <marzoukagrignan@gmail.com>
2025-06-22 00:38:38 +0200

    'lastecommit'

--- src/Home.jsx
+++ src/Home.jsx
@@ -122,8 +122,8 @@ export function Home() {
           <div style={{
             position: "absolute",
             top: 0, left: 0, right: 0, bottom: 0,
-               clipPath: path("M 0 303.5 C 0 292.454 8.995 285.101 20 283.5 L 460 219.5 C 470.085 218.033 480 228.454 480 239.5 L 500 430 C 500 441.046 491.046 450 480 450 L 20 450 C 8.954 450 0 441.046 0 430 Z"),
-            background: `linear-gradient(306deg, #FF05B4, hsl(10, 100%, 50%))`
+              clipPath: 'path("M 0 303.5 C 0 292.454 8.995 285.101 20 283.5 L 460 219.5 C 470.085 218.033 480 228.454 480 239.5 L 500 430 C 500 441.046 491.046 450 480 450 L 20 450 C 8.954 450 0 441.046 0 430 Z")',
+               background: `linear-gradient(306deg, #FF05B4, hsl(10, 100%, 50%))`
           }} />
 
           {/* Carte contenant l’image */}
